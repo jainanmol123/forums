a8d31ec3c298aae4af140fce8c0d7b9172238bf9
HargovindArora <hargovinds50@gmail.com>
2018-03-30 11:25:04 +0530

    404 handler view new urls mapped and corresponding tests

--- boards/views.py
+++ boards/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from .models import Board
 
 # Create your views here.
@@ -9,5 +10,5 @@ def home(request):
 
 
 def board_topics(request, pk):
-    board = Board.objects.get(pk=pk)
+    board = get_object_or_404(Board, pk=pk)
     return render(request, 'topics.html', {'board':board})
