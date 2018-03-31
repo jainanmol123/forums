fb93848bf4c449874ba3c0ff5b8b5aab69dc1362
HargovindArora <hargovinds50@gmail.com>
2018-03-31 10:26:39 +0530

    integrated forms in views

--- boards/views.py
+++ boards/views.py
@@ -17,25 +17,23 @@ def board_topics(request, pk):
 
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-
+    user = User.objects.first()
     if request.method == 'POST':
-        subject = request.POST['subject']
-        message = request.POST['message']
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
 
-        user = User.objects.first()
+            return redirect('board_topics', pk=board.pk)
 
-        topic = Topic.objects.create(
-            subject=subject,
-            board=board,
-            starter=user
-        )
+        else:
+            form = NewTopicForm()
 
-        post = Post.objects.create(
-            message=message,
-            topic=topic,
-            created_by=user
-        )
-
-        return redirect('board_topics', pk=board.pk)
-
-    return render(request, 'new_topic.html', {'board': board})
+    return render(request, 'new_topic.html', {'board': board, 'form':form})
