3307baeb5182cf838fe5830c4776c346806f118e
HargovindArora <hargovinds50@gmail.com>
2018-04-08 14:53:01 +0530

    basic signup view, its tests and html page

--- myproject/urls.py
+++ myproject/urls.py
@@ -21,7 +21,7 @@ from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
-    url(r'^signup/$', accounts_views.signup, name='signup')
+    url(r'^signup/$', accounts_views.signup, name='signup'),
     url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^boards/(?P<pk>\d+)/new/$', views.new_topic, name='new_topic'),
     url(r'^admin/', admin.site.urls),
