73f716eadd89fa0f3cde5e8f45427fb6b8194c7f
HargovindArora <hargovinds50@gmail.com>
2018-04-16 15:05:28 +0530

    permissions changed for edit post

--- boards/views.py
+++ boards/views.py
@@ -7,15 +7,20 @@ from django.db.models import Count
 from django.shortcuts import redirect
 from django.views.generic import UpdateView
 from django.utils import timezone
+from django.utils.decorators import method_decorator
 
 # Create your views here.
-
+@method_decorator(login_required, name='dispatch')
 class PostUpdateView(UpdateView):
     model = Post
     fields = ('message', )
     template_name = 'edit_post.html'
     pk_url_kwarg = 'post_pk'
     context_object_name = 'post'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(created_by=self.request.user)
 
     def form_valid(self, form):
         post = form.save(commit=False)
