0102138efa5aa9168ee12a6bd7bc4a7df29868de
HargovindArora <hargovinds50@gmail.com>
2018-04-17 23:47:08 +0530

    basic functionality improved

--- boards/views.py
+++ boards/views.py
@@ -9,6 +9,7 @@ from django.views.generic import UpdateView, ListView
 from django.utils import timezone
 from django.utils.decorators import method_decorator
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.urls import reverse
 
 # Create your views here.
 @method_decorator(login_required, name='dispatch')
@@ -85,7 +86,14 @@ def reply_topic(request, pk, topic_pk):
             topic.last_updated = timezone.now()
             topic.save()
 
-            return redirect('topic_posts', pk=pk, topic_pk=topic_pk)
+            topic_url = reverse('topic_posts', kwargs={'pk': pk, 'topic_pk': topic_pk})
+            topic_post_url = '{url}?page={page}#{id}'.format(
+                url=topic_url,
+                id=post.pk,
+                page=topic.get_page_count()
+            )
+
+            return redirect(topic_post_url)
     else:
         form = PostForm()
     return render(request, 'reply_topic.html', {'topic':topic, 'form': form})
@@ -99,11 +107,11 @@ class PostListView(ListView):
     def get_context_data(self, **kwargs):
 
         session_key = 'viewed_topic_{}'.format(self.topic.pk)
-        if noy self.request.session.get(session_key, False):
+        if not self.request.session.get(session_key, False):
             self.topic.views += 1
             self.topic.save()
             self.request.session[session_key] = True
-                
+
         kwargs['topic'] = self.topic
         return super().get_context_data(**kwargs)
 
